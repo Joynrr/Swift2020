0a85eedb37365f14864d692d517292c7a8693240
Joynrr <taoqiaonan@gmail.com>
2020-08-17 14:53:11 -0700

    Lecture 1 Course Logistics and Intro to SwiftUI

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        return HStack{
+//            Text("hello") add one more column
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool 
+    var body: some View{
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)// Override ZStack Color value
+                Text("👻")
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+
+        }
     }
 }
 
