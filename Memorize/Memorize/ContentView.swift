7f471abd68f69a61d1f1c4f1a3ee414faaca1f5f
Joynrr <taoqiaonan@gmail.com>
2020-08-17 13:43:31 -0700

    init

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Qiaonan Tao on 8/17/20.
+//  Copyright © 2020 CS193p Joynrr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
